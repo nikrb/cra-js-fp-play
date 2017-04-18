8113f7e62e87e57ab79668da47853dfc6788f565
nikrb <nik_rb@yahoo.com>
2017-04-18 19:01:36 +0100

    fixup env for playing with js with console

---
+++ src/data.js
@@ -0,0 +1,16 @@
+
+const lists = [
+  { created: "2017-04-10",
+    foods: [
+      { name: "onion", cost: "0.23"},
+      { name: "pepper", cost: "0.5"}
+    ]},
+  { created: "2017-04-11",
+    foods: [
+      { name: "onion", cost: "0.25"},
+      { name: "potatoes", cost: "0.12"}
+    ]
+  }
+];
+
+export default lists;
