9a8f76abd8671c436a555fc884c321af3980f678
nikrb <nik_rb@yahoo.com>
2017-04-18 20:33:38 +0100

    implement exercise-19

--- src/play.js
+++ src/play.js
@@ -1,20 +1,21 @@
 import './concatAll';
 import data from './data';
 
-// id, title and 150x200 boxart
-// example uses map and concatAll at end, it seems concatMap would work
-	const out = data
-	  .concatMap(function(movieList) {
-      return movieList.videos
-        .concatMap(function(video) {
-          return video.boxarts
-            .filter(function(boxart) {
-            	return boxart.width === 150;
-            })
-            .map(function(boxart) {
-            	return {id: video.id, title: video.title, boxart: boxart.url};
-            });
-        });
-	  });
+	// Expecting this output...
+	// [
+	//	 {
+	//		 "65432445": "The Chamber",
+	//		 "675465": "Fracture",
+	//		 "70111470": "Die Hard",
+	//		 "654356453": "Bad Boys"
+	//	 }
+	// ]
+
+
+	const out = data.reduce( function( acc, cur){
+    const obj = {};
+    obj[cur.id] = cur.title;
+    return {...acc, ...obj}; // Object.assign( acc, obj);
+  }, {});
 
 console.log( out);
