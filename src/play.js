5822c56179afc3c87604b269c06d4d7fd2cabf74
nikrb <nik_rb@yahoo.com>
2017-04-18 22:25:21 +0100

    implement exercise 24

--- src/play.js
+++ src/play.js
@@ -1,17 +1,22 @@
 import './concatAll';
 import data from './data';
 
-// id, title and smallest box art url
+// id, title, middle interesting moment time and smallest box art url
 const out = data.map( function( movieList){
   return movieList.videos.map( function( video){
-    return video.boxarts.reduce( function( min, boxart){
-      if(boxart.width < min.width) return boxart;
-      return min;
-    })
-    .map( function( boxart){
-      return { id: video.id, title: video.title,
-        boxart: boxart.url};
-    });
+    return Array.zip(
+      video.boxarts.reduce( function( min, boxart){
+        if(boxart.width < min.width) return boxart;
+        return min;
+      }),
+      video.interestingMoments.filter( function( im){
+        return im.type === "Middle";
+      }),
+      function( boxart, int_mom){
+        return {id:video.id, title:video.title,
+          interestingMoment: int_mom.time,
+          boxart: boxart.url}
+      });
   }).concatAll();
 }).concatAll();
 
