a0ef4b10988d1a679ad83f89dac796f1bf6be6ee
nikrb <nik_rb@yahoo.com>
2017-04-18 23:49:27 +0100

    implement exercise 27

--- src/data.js
+++ src/data.js
@@ -1,50 +1,10 @@
-
-	export const lists = [
-			{
-				"id": 5434364,
-				"name": "New Releases"
-			},
-			{
-				"id": 65456475,
-				name: "Thrillers"
-			}
-		];
-export const videos = [
-			{
-				"listId": 5434364,
-				"id": 65432445,
-				"title": "The Chamber"
-			},
-			{
-				"listId": 5434364,
-				"id": 675465,
-				"title": "Fracture"
-			},
-			{
-				"listId": 65456475,
-				"id": 70111470,
-				"title": "Die Hard"
-			},
-			{
-				"listId": 65456475,
-				"id": 654356453,
-				"title": "Bad Boys"
-			}
-		];
-export const boxarts = [
-			{ videoId: 65432445, width: 130, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg" },
-			{ videoId: 65432445, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" },
-			{ videoId: 675465, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
-			{ videoId: 675465, width: 120, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture120.jpg" },
-			{ videoId: 675465, width: 300, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" },
-			{ videoId: 70111470, width: 150, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
-			{ videoId: 70111470, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard200.jpg" },
-			{ videoId: 654356453, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg" },
-			{ videoId: 654356453, width: 140, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg" }
-		];
-export const bookmarks = [
-			{ videoId: 65432445, time: 32432 },
-			{ videoId: 675465, time: 3534543 },
-			{ videoId: 70111470, time: 645243 },
-			{ videoId: 654356453, time: 984934 }
-		];
+const pricesNASDAQ = [
+	// ... from the NASDAQ's opening day
+	{name: "ANGI", price: 31.22, timeStamp: new Date(2011,11,15) },
+	{name: "MSFT", price: 32.32, timeStamp: new Date(2011,11,15) },
+	{name: "GOOG", price: 150.43, timeStamp: new Date(2011,11,15)},
+	{name: "ANGI", price: 28.44, timeStamp: new Date(2011,11,16)},
+	{name: "GOOG", price: 199.33, timeStamp: new Date(2011,11,16)},
+	// ...and up to the present.
+];
+export default pricesNASDAQ;
--- src/play.js
+++ src/play.js
@@ -1,32 +1,9 @@
 import './concatAll';
-import {lists, videos, boxarts, bookmarks} from './data';
+import data from './data';
 
 // id, title, bookmark time, smallest boxart
-const out = lists.map( function( list){
-  return {
-    name: list.name,
-    videos: videos.filter( function( video){
-      return video.listId === list.id;
-    }).map( function( video){
-      return {
-        id: video.id,
-        title: video.title,
-        time: bookmarks.filter( function( bm){
-            return bm.videoId === video.id;
-          }).map( function( bm){
-            return bm.time;
-          })[0],
-        boxart: boxarts.filter( function( boxart){
-            return boxart.videoId === video.id;
-          }).reduce( function( acc, cur){
-            if( cur.width < acc.width) return cur;
-            return acc;
-          }).map( function( ba){
-            return ba.url;
-          })[0]
-      };
-    })
-  };
+const out = data.filter( function( price){
+  return price.name === "MSFT";
 });
 
 console.log( out);
