4d45e5a1cea09095f344eafa62bf26d2114dc232
nikrb <nik_rb@yahoo.com>
2017-04-18 19:13:56 +0100

    tweak readme with fp web site exercises and remove old comment

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 simple react to play with javascript functional programming, just using the console, without any html, just console output.
+
+[functional programming in javascript](http://reactivex.io/learnrx/)
--- src/concatAll.js
+++ src/concatAll.js
@@ -3,9 +3,6 @@
 Array.prototype.concatAll = function() {
 	var results = [];
 	this.forEach(function(subArray) {
-		// ------------ INSERT CODE HERE! ----------------------------
-		// Add all the items in each subArray to the results array.
-		// ------------ INSERT CODE HERE! ----------------------------
     subArray.forEach( function( item){
       results.push( item);
     });
